fdb58b8f99d84de711e0bbc20f3795f6458d64fa
Vinicius Salum <50721619+viniciussalum0405@users.noreply.github.com>
2025-11-07 17:39:15 -0300

    Update solution - CodeHub

--- 8-kyu/fix-your-code-before-the-garden-dies/fix-your-code-before-the-garden-dies.py
+++ 8-kyu/fix-your-code-before-the-garden-dies/fix-your-code-before-the-garden-dies.py
@@ -2,6 +2,6 @@
 def rain_amount(mm):
     if mm < 40:
         mm = 40 - mm
-        return f"You need to give your plant {mm} mm of water"
+        return f"You need to give your plant {mm}mm of water"
     else:
         return "Your plant has had more than enough water for today!"
